40fd6a62d6ad797dae3d7a0d242303a4c6a80a2b
Wang Zhengyang <wzhyno1@163.com>
2016-11-14 11:50:32 +0800

    使用ButterKnife的BindView()和OnClick()完成点击改变TextView的文字

--- app/src/main/java/com/techfit/wzhy/butterknifedemo/MainActivity.java
+++ app/src/main/java/com/techfit/wzhy/butterknifedemo/MainActivity.java
@@ -2,12 +2,30 @@ package com.techfit.wzhy.butterknifedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv)
+    TextView mTv;
+    private int mCount = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.btn)
+    void changeText() {
+        mTv.setText("按钮被点击了" + (++mCount) + "次");
     }
 }
